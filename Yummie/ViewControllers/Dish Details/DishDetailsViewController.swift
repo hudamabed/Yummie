40f3a72f0632dc6d2ceef31bcbd2f4074c88e39f
Huda M. A'abed <hudamabed@gmail.com>
2023-09-09 19:13:49 +0300

    Dish Details View Controller

---
+++ Yummie/ViewControllers/Dish Details/DishDetailsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DishDetailsViewController.swift
+//  Yummie
+//
+//  Created by Huda M. A'abed on 09/09/2023.
+//
+
+import UIKit
+
+class DishDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupData()
+        localized()
+        fetchData()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
+
+extension DishDetailsViewController {
+    func setupView(){
+    }
+    
+    func localized(){
+    }
+    
+    func setupData(){
+    }
+    
+    func fetchData(){
+    }
+}
+
+extension DishDetailsViewController {
+    
+    
+    
+}
+
